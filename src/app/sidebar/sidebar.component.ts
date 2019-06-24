0831ef860663532e9de11904826edf016f3053f3
Muhammad Junaid <muhammadjunaidhassan1@gmail.com>
2019-06-24 17:36:10 +0500

    checks

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor(private api:ApiService,private router:Router) { }
+
+  ngOnInit() {
+  }
+
+logOut(){
+  localStorage.removeItem('uid');
+  this.router.navigate(['/landing'])
+  
+}
+
+  sidebar=[
+    {icon:'dashboard', url:'/dashboard/home', text:'Dashboard'},
+    {icon:'person', url:'/dashboard/profile', text:'Profile'},
+    // {icon:'local_pharmacy', url:'/dashboard/vaccines', text:'Vaccines'},
+    // {icon:'wc', url:'/dashboard/guardians', text:'Guardians'},
+    {icon:'nature_people', url:'/dashboard/workers', text:'Workers'},
+    {icon:'local_hospital', url:'/dashboard/conversations', text:'Conversations'},
+    {icon:'wc', url:'/dashboard/notifications', text:'Notifications'},
+
+    // {icon:'exit_to_app', url:'/dashboard/logout', text:'Logout'},
+  ]
+}
